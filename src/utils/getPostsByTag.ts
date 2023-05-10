eb0926c418cdf1df04f58268c55bac13876b240b
Zicklag <zicklag@katharostech.com>
2023-05-10 17:42:44 -0500

    feat: sort posts on tag page.

--- src/utils/getPostsByTag.ts
+++ src/utils/getPostsByTag.ts
@@ -2,6 +2,12 @@ import { slugifyAll } from "./slugify";
 import type { CollectionEntry } from "astro:content";
 
 const getPostsByTag = (posts: CollectionEntry<"blog">[], tag: string) =>
-  posts.filter(post => slugifyAll(post.data.tags).includes(tag));
+  posts
+    .filter(post => slugifyAll(post.data.tags).includes(tag))
+    .sort(
+      (a, b) =>
+        Math.floor(new Date(b.data.pubDatetime).getTime() / 1000) -
+        Math.floor(new Date(a.data.pubDatetime).getTime() / 1000)
+    );
 
 export default getPostsByTag;
